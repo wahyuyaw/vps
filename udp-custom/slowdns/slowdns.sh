62676ca1429261bc9acd21d88ffd14c8d9fc0156
wahyuyaw <yawwahyu@gmail.com>
2025-02-08 23:55:09 +0700

    Update slowdns.sh

--- udp-custom/slowdns/slowdns.sh
+++ udp-custom/slowdns/slowdns.sh
@@ -1,7 +1,7 @@
 # =========================================
 # Quick Setup | Script Setup Manager
 # Edition : Stable Edition 1.0
-# Auther  : givpn
+# Auther  : wahyuyaw
 # Kuota Habisss
 # (C) Copyright 2023
 # =========================================
