d0955ea0c3b9fdeed5285459e9154c4490a05a3e
wahyuyaw <yawwahyu@gmail.com>
2025-04-01 18:42:51 +0700

    Update ins-helium.sh

--- helium/ins-helium.sh
+++ helium/ins-helium.sh
@@ -2,7 +2,7 @@
 # =========================================
 # Quick Setup | Script Setup Manager
 # Edition : Stable Edition 1.0
-# Auther  : givpn
+# Auther  : wahyuyaw
 # Kuota Habisss
 # (C) Copyright 2023
 # =========================================
